1ea0249120303fe4e304709b76082e2073175c20
purps <goglqbmo@gmail.com>
2021-12-20 16:36:41 +0300

    new migrations

---
+++ FashopBackend.Infrastructure/Migrations/20211218165320_user-token-column-nullable.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FashopBackend.Infrastructure.Migrations
+{
+    public partial class usertokencolumnnullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "token",
+                table: "users",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "token",
+                table: "users",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
---
+++ FashopBackend.Infrastructure/Migrations/20211220125701_added-roles-table.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FashopBackend.Infrastructure.Migrations
+{
+    public partial class addedrolestable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "roleId",
+                table: "users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "roles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_roles", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_roleId",
+                table: "users",
+                column: "roleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_roles_name",
+                table: "roles",
+                column: "name",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_users_roles_roleId",
+                table: "users",
+                column: "roleId",
+                principalTable: "roles",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_users_roles_roleId",
+                table: "users");
+
+            migrationBuilder.DropTable(
+                name: "roles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_users_roleId",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "roleId",
+                table: "users");
+        }
+    }
+}
