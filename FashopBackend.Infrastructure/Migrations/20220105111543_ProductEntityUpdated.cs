af18f914fb808de95b522995e47183d8c85a7138
purps <goglqbmo@gmail.com>
2022-01-05 17:56:17 +0300

    Product Entity Migrations

---
+++ FashopBackend.Infrastructure/Migrations/20220105111543_ProductEntityUpdated.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FashopBackend.Infrastructure.Migrations
+{
+    public partial class ProductEntityUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "description",
+                table: "products",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "price",
+                table: "products",
+                type: "money",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "description",
+                table: "products");
+
+            migrationBuilder.DropColumn(
+                name: "price",
+                table: "products");
+        }
+    }
+}
